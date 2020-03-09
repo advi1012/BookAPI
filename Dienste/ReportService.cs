7d27a0b545187c139dffd37a479f0d7cfba72e52
Vincent Adam <Vincent.Adam@home.cas.de>
2020-03-09 17:48:21 +0100

    Reportservice

--- Dienste/ReportService.cs
+++ Dienste/ReportService.cs
@@ -7,5 +7,37 @@ namespace BooksApi.Dienste
 {
     public class ReportService
     {
+        private readonly IMongoCollection<Report> _report;
+
+        public ReportService(IReportstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _report = database.GetCollection<Report>(settings.ReportsCollectionName);
+        }
+
+        public List<Report> Get() =>
+            _report.Find<Report>(report => true).ToList();
+
+        public Report GetById(string id) =>
+            _report.Find(report => report.Id == id).FirstOrDefault();
+
+        public Report Create(Report report)
+        {
+            _report.InsertOne(report);
+            return report;
+
+        }
+
+        public void Update(string id, Report report)
+        {
+            _report.ReplaceOne(report => report.Id == id, report);
+        }
+
+        public void Remove(Report reportIn) =>
+            _report.DeleteOne(report => report.Id == report.Id);
+
+        public void Remove(string id) => _report.DeleteOne(report => report.Id == id);
     }
 }
